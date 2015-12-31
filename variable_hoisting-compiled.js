4d1e43fae6a8a552ebc83f24bd4f92c070c40eec
Uche Eke <uche.eke@gmail.com>
2015-12-31 00:59:37 +0000

    Variable hoisting

--- variable_hoisting-compiled.js
+++ variable_hoisting-compiled.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Created by ekeu on 31/12/15.
  */
@@ -8,3 +10,5 @@ elem = document.getElementById("hoistedVar");
 elem.innerHTML = x;
 
 var x; // Declared AFTER use
+
+//# sourceMappingURL=variable_hoisting-compiled.js.map
